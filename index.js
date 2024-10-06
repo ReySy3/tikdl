f22518788cf92b570c30268fc1bcf40035290b89
ReySy3 <149598631+ReySy3@users.noreply.github.com>
2024-10-06 20:28:02 +0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const apiUrl = "https://api.kenliejugarap.com/tikwmbymarjhun/?url=${encodeURIComponent(url)"
-fetch (apiUrl)
-.then(response =>response.json())
-.then(data>{
-  console.log(data);
-{
